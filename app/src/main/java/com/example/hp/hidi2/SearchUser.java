b7cab50c955dbe2c06a0ec0322b1119cb7bf4680
adityabajpai <adityabajpai0303@gmail.com>
2018-03-27 19:14:08 +0530

    adi commit search new user

--- app/src/main/java/com/example/hp/hidi2/SearchUser.java
+++ app/src/main/java/com/example/hp/hidi2/SearchUser.java
@@ -1,17 +1,140 @@
 package com.example.hp.hidi2;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SearchUser extends AppCompatActivity {
 
-    SearchView.SearchAutoComplete searchAutoComplete;
+    //    SearchView.SearchAutoComplete searchAutoComplete;
+    SearchView searchAutoComplete;
+    String result = "";
+    SessionManager session;
+    HashMap<String, Integer> hashMap = new HashMap<>();
+    ArrayList<String> username = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_user);
+
         searchAutoComplete = findViewById(R.id.searchuser);
+//        searchAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String click_String = (String) parent.getItemAtPosition(position);
+//            }
+//        });
+        session = new SessionManager(getApplicationContext());
+        session.checkLogin();
+        new Posts().execute("http://hidi.org.in/hidi/account/showvisitors.php");
+    }
+
+    public String POST(String url) {
+        InputStream inputStream = null;
+        try {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+            String json = "";
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("uid", session.getUID());
+            jsonObject.accumulate("request", "all");
+            json = jsonObject.toString();
+            json = jsonObject.toString();
+            Log.d("Json", json);
+            StringEntity se = new StringEntity(json);
+            Log.d("Entity", "" + se);
+            httpPost.setEntity(se);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            Log.d("Post", "" + httpPost);
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            Log.d("Response", httpResponse.toString());
+            inputStream = httpResponse.getEntity().getContent();
+            Log.d("inputStream", inputStream.toString());
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null)
+            result += line;
+        inputStream.close();
+        return result;
+    }
+
+    private class Posts extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            return POST(urls[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("result", result);
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject info = jsonObject.getJSONObject("info");
+                if (info.getString("status").equals("success")) {
+                    JSONArray details = jsonObject.getJSONArray("details");
+                    for (int i = 0; i < details.length(); i++) {
+                        JSONObject user = details.getJSONObject(i);
+                        hashMap.put(user.getString("secname"), user.getInt("uid"));
+                        username.add(user.getString("secname"));
+                    }
+                } else {
+                    Toast.makeText(SearchUser.this, "Unable to find User", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
